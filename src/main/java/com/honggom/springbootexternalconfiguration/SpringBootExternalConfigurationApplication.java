9b5164237194e37dcfc07f9d850c5913e6c0c1d1
h0ngg0m <jaehongbyeon98@gmail.com>
2024-12-10 22:49:35 +0900

    :tada: init

---
+++ src/main/java/com/honggom/springbootexternalconfiguration/SpringBootExternalConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.honggom.springbootexternalconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExternalConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExternalConfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/honggom/springbootexternalconfiguration/SpringBootExternalConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honggom.springbootexternalconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExternalConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
